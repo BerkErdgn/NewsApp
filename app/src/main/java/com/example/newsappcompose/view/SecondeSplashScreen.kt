764989e038f0c4de7602fbc5b273c6e749d37fa9
Berk Erdogan <121022274+BerkErdgn@users.noreply.github.com>
2023-03-25 20:41:03 +0300

    Splash and Lottie
    Splash screen and Lottie completed

--- app/src/main/java/com/example/newsappcompose/view/SecondeSplashScreen.kt
+++ app/src/main/java/com/example/newsappcompose/view/SecondeSplashScreen.kt
@@ -2,37 +2,90 @@ package com.example.newsappcompose.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.newsappcompose.graphs.AuthScreen
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.newsappcompose.graphs.Graph
+import com.example.newsappcompose.ui.theme.custemPink
+import com.example.newsappcompose.ui.theme.customBlur
+
 
 @Composable
 fun SecondeSplashScreen(navController: NavHostController){
 
-    Text(
-        modifier = Modifier
-            .clickable {
-                navController.popBackStack()
-                navController.navigate(Graph.HOME)
-            }
-            .background(color = Color.Blue)
-            .padding(start = 5.dp, top = 8.dp, bottom = 15.dp)
-            // .fillMaxSize(0.5f)
+    Column (modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        //For LottieAnimation
+
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url("https://assets2.lottiefiles.com/packages/lf20_2LdLki.json"))
+        LottieAnimation(composition = composition, iterations =LottieConstants.IterateForever )
+
+        Spacer(modifier = Modifier.padding(3.dp))
+
+        //For Text
+        val preamble = "You have come to the right place to learn the latest and accurate news. Come in."
+        Text(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 5.dp, top = 5.dp, end = 5.dp)
             .width(200.dp)
-        ,text = "Devam",
-        color = Color.Red,
-        fontSize = 25.sp,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
-    )
+            , text = preamble,
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center
+            )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+
+
+        //For button
+        Button(onClick = {
+            navController.popBackStack()
+            navController.navigate(Graph.HOME)
+        },
+            colors = ButtonDefaults.buttonColors(backgroundColor = custemPink),
+            shape = CutCornerShape(10.dp),
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 30.dp,
+                pressedElevation = 25.dp,
+                disabledElevation = 0.dp
+            ),
+            modifier = Modifier
+                .size(width = 150.dp, height = 35.dp)
+        ) {
+            Text(text = "Continue",
+                color = Color.White)
+        }
+
+    }
+
 }
+//For Preview
+/*
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    SecondeSplashScreen()
+}
+
+ */
